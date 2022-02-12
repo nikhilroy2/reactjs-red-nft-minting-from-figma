61ae6e3c73162aaff63ddc5e32666b35c247a919
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-02-12 23:45:23 +0600

    homepage first section working

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,7 +1,32 @@
 import React from "react";
 import "./Homepage.css";
 function Homepage(props) {
-  return <div id="Homepage">Discover Rare Collection of NFT</div>;
+  return (
+    <div id="Homepage">
+      <Section1></Section1>
+    </div>
+  );
 }
 
 export default Homepage;
+
+function Section1() {
+  return (
+    <section id="Section1">
+      <div className="section_container">
+        <div className="row g-0">
+          <div className="col-md-6 ">
+            <h1 className="text-uppercase">Discover Rare Collection of NFT</h1>
+            <p>
+              “Everything or nothing. Here I am, in front of me is the World of
+              Art - there is no turning back and I will force myself to love.”
+            </p>
+          </div>
+          <div className="col-md-6 ">
+            <img className="presale_img" src={require("../../Static/img/presale_img.png")} alt="img" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
