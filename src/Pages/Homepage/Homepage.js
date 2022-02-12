dd164b758a6ac0b2cdc0b50a93eeaa29231b88ce
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-02-12 22:40:11 +0600

    header

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import './Homepage.css';
+import React from "react";
+import "./Homepage.css";
 function Homepage(props) {
-    return (
-        <div id='Homepage'>
-            
-        </div>
-    );
+  return <div id="Homepage">Discover Rare Collection of NFT</div>;
 }
 
 export default Homepage;
